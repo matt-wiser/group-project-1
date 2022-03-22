adf4dc8c1b9df12c9c7e3d5895481a98a0145344
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-03-22 15:56:02 -0500

    very basic HTML framework

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
-var npsKey = config.npsKey;
-var queryUrl = config.queryUrl;
+//var npsKey = config.npsKey;
+//var queryUrl = config.queryUrl;
 var npsURL = 'developer.nps.gov/api/v1'
 
-fetch(queryUrl)
-.then(function(response){
+var gMapsURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDqnsrJI6glSMm-yvEVcj5bOFO5zK-RsYo&callback=initMap&v=weekly"
+
+// fetch(queryUrl)
+// .then(function(response){
+//     response = response.json();
+//     return response;
+// })
+// .then(function(response){
+//     console.log(response.data);
+// })
+
+fetch (gMapsURL)
+.then(function(response) {
     response = response.json();
     return response;
 })
-.then(function(response){
+.then(function(response) {
     console.log(response.data);
 })
