50f4edd8c5aea81f6ae4f91e3d597d53ac58dcc6
Matthew Wiser <matt.wiser@icloud.com>
2022-03-22 14:20:51 -0500

    split out nps call feature into a separate js file

---
+++ assets/js/nps-call.js
@@ -0,0 +1,12 @@
+var npsKey = 'Zn4OQSperFdVsW4h6jkMEi8SKODcfpVLsQ43wFqA';
+var queryUrl = 'https://developer.nps.gov/api/v1/parks?stateCode=TN&api_key=Zn4OQSperFdVsW4h6jkMEi8SKODcfpVLsQ43wFqA';
+var npsURL = 'developer.nps.gov/api/v1'
+
+fetch(queryUrl)
+.then(function(response){
+    response = response.json();
+    return response;
+})
+.then(function(response){
+    console.log(response.data);
+})
