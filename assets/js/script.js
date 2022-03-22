b101f14b32794c2a2915c89e81ffa8a7775703d1
Matthew Wiser <matt.wiser@icloud.com>
2022-03-22 11:26:57 -0500

    made NPS API key available in main script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-var npsKey = config.npsKey;
-var queryUrl = config.queryUrl;
+var npsKey = 'Zn4OQSperFdVsW4h6jkMEi8SKODcfpVLsQ43wFqA';
+var queryUrl = 'https://developer.nps.gov/api/v1/parks?stateCode=TN&stateCode=&api_key=Zn4OQSperFdVsW4h6jkMEi8SKODcfpVLsQ43wFqA';
 var npsURL = 'developer.nps.gov/api/v1'
 
 fetch(queryUrl)
