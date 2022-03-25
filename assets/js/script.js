6a7adfb05f6af9a86ab453dd3238201ca5de61d9
Matthew Wiser <matt.wiser@icloud.com>
2022-03-25 09:26:32 -0500

    commented out script.js so code doesn't execute twice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,61 @@
-var npsUrl = 'https://developer.nps.gov/api/v1/parks?api_key=Zn4OQSperFdVsW4h6jkMEi8SKODcfpVLsQ43wFqA'
+// var npsUrl = 'https://developer.nps.gov/api/v1/parks?api_key=Zn4OQSperFdVsW4h6jkMEi8SKODcfpVLsQ43wFqA'
 
-function getFormData(){
-    var selectedState = document.getElementById('state-select');
-    selectedState = selectedState.value;
+// function getFormData(){
+//     var selectedState = document.getElementById('state-select');
+//     selectedState = selectedState.value;
 
-    var streetAddress = document.getElementById('address');
-    streetAddress = streetAddress.value;
+//     var streetAddress = document.getElementById('address');
+//     streetAddress = streetAddress.value;
 
-    var city = document.getElementById('city');
-    city = city.value;
+//     var city = document.getElementById('city');
+//     city = city.value;
 
-    var zipCode = document.getElementById('zip');
-    zipCode = zipCode.value;
+//     var zipCode = document.getElementById('zip');
+//     zipCode = zipCode.value;
 
-    var searchData = {
-        selectedState: selectedState,
-        streetAddress: streetAddress,
-        city: city,
-        zipCode: zipCode,
-    }
-    console.log(searchData);
-    submitRequest(searchData);
-}
+//     var searchData = {
+//         selectedState: selectedState,
+//         streetAddress: streetAddress,
+//         city: city,
+//         zipCode: zipCode,
+//     }
+//     console.log(searchData);
+//     submitRequest(searchData);
+// }
 
-function constructQueryUrl(searchData){
-    var queryUrl = npsUrl + '&stateCode=' + searchData.selectedState;
-    return queryUrl;
-}
+// function constructQueryUrl(searchData){
+//     var queryUrl = npsUrl + '&stateCode=' + searchData.selectedState;
+//     return queryUrl;
+// }
 
-function submitRequest(searchData){
-    var queryUrl = constructQueryUrl(searchData);
+// function submitRequest(searchData){
+//     var queryUrl = constructQueryUrl(searchData);
     
-    fetch(queryUrl)
-    .then(function(response){
-        response = response.json();
-        return response;
-    })
-    .then(function(response){
-        updateView(response.data);
-        latLongcalc(response.data);
-        console.log(response.data);
-    })
-}
-function latLongcalc(parkData) {
-    var lat = parkData[0].latitude;
-    var long = parkData[0].longitude;
-    fetch("https://api.sunrise-sunset.org/json?lat=" + lat + "&lng=" + long)
-    .then(function(response){
-        response = response.json();
-        return response;
-    }).then(function(response){
-        console.log(response);
-    })
-}
+//     fetch(queryUrl)
+//     .then(function(response){
+//         response = response.json();
+//         return response;
+//     })
+//     .then(function(response){
+//         updateView(response.data);
+//         latLongcalc(response.data);
+//         console.log(response.data);
+//     })
+// }
+// function latLongcalc(parkData) {
+//     var lat = parkData[0].latitude;
+//     var long = parkData[0].longitude;
+//     fetch("https://api.sunrise-sunset.org/json?lat=" + lat + "&lng=" + long)
+//     .then(function(response){
+//         response = response.json();
+//         return response;
+//     }).then(function(response){
+//         console.log(response);
+//     })
+// }
 
-function updateView(npsData){
+// function updateView(npsData){
 
-}
+// }
 
-$('#submitaddress').click(getFormData);
+// $('#submitaddress').click(getFormData);
